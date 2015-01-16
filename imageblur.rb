d98a54253fbcca65bf00fd976f0fb78f9ec5ec5f
Erin Kim <erin.e.kim@gmail.com>
2015-01-16 04:46:13 +0000

    Solve Image Blur 1 problem.

---
+++ imageblur.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+
+class Image
+	attr_accessor :image
+
+	def initialize(image) 
+		@image = image
+	end 
+
+	
+	def output_image 
+		@image.each do |array|
+		puts array.join("")
+		end
+	end
+
+end 
+
+class TestImage < MiniTest::Test
+	def test_output_image
+		image = Image.new([
+			[0,0,0,0],
+			[0,1,0,0],
+			[0,0,0,1],
+			[0,0,0,0]
+		])
+
+		assert image.output_image 
+		expected = [
+			[0,0,0,0],
+			[0,1,0,0],
+			[0,0,0,1],
+			[0,0,0,0]
+		]
+
+		assert_equal expected, image 
+	end 
+
+end 
